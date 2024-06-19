844614246533f211f94a2345a17a26a720bd3cef
Onni Karjalainen <ontajoka@jyu.fi>
2024-06-19 12:18:05 +0300

    Added classes to models

--- task_app/models.py
+++ task_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    task_description = models.CharField(max_length=100)
+    task_date = models.DateField()
+    
+class TaskList(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return reverse("list", args=[self.id])
