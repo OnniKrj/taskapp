a26ef398a692bc2681017899abd6033b85f19a90
Onni Karjalainen <ontajoka@jyu.fi>
2024-06-19 14:31:20 +0300

    Implemented Read, created index and task list views

--- task_app/urls.py
+++ task_app/urls.py
@@ -1,4 +1,8 @@
 # taskapp/task_app/urls.py
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
 ]
--- task_app/views.py
+++ task_app/views.py
@@ -1,6 +1,20 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView
-from .models import TaskList
+from .models import TaskList, Task
 
 class ListListView(ListView):
     model = TaskList
     template_name = "task_app/index.html"
+    
+class ItemListView(ListView):
+    model = Task
+    template_name = "task_app/task_list.html"
+    
+    def get_queryset(self):
+        return Task.objects.filter(todo_list_id=self.kwargs["list_id"])
+    
+    def get_context_data(self):
+        context =  super().get_context_data()
+        context["task_list"] = TaskList.objects.get(id=self.kwargs["list_id"])
+        return context
