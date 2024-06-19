f9d322fd7a9f1840c141e068e8cfd66b60a6aae2
Onni Karjalainen <ontajoka@jyu.fi>
2024-06-19 12:31:08 +0300

    Updated models, added methods for managing data

--- task_app/models.py
+++ task_app/models.py
@@ -2,15 +2,32 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
-# Create your models here.
-
-class Task(models.Model):
-    task_name = models.CharField(max_length=50)
-    task_description = models.CharField(max_length=100)
-    task_date = models.DateField()
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
     
 class TaskList(models.Model):
     title = models.CharField(max_length=50, unique=True)
     
-    def __str__(self):
+    def get_absolute_url(self):
         return reverse("list", args=[self.id])
+      
+    def __str__(self):
+        return self.title
+    
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(default=one_week_hence)
+    todo_list = models.ForeignKey(TaskList, on_delete=models.CASCADE)
+
+    def get_absolute_url(self):
+        return reverse(
+            "item-update", args=[str(self.todo_list.id), str(self.id)]
+        )
+    
+    def __str__(self):
+        return f"{self.title}: due {self.due_date}"
+    
+    class Meta:
+        ordering = ["due_date"]
