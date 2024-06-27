48ec466015185adc9bd5ce92b22cc21be2306ea9
Onni Karjalainen <ontajoka@jyu.fi>
2024-06-27 14:20:57 +0300

    Added forms for task and tasklist deletion

--- task_app/urls.py
+++ task_app/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
     # CRUD patterns for TaskLists
     path("list/add/", views.ListCreate.as_view(), name="list-add"),
+    path("list/<int:pk>/delete/", views.TaskListDelete.as_view(), name="list-delete"),
     # CRUD patterns for Tasks
     path("list/<int:list_id>/item/add/", views.TaskCreate.as_view(), name="item-add"),
     path("list/<int:list_id>/item/<int:pk>/", views.TaskUpdate.as_view(), name="item-update"),
+    path("list/<int:list_id>/item/<int:pk>/delete/", views.TaskDelete.as_view(), name="item-delete",),
 ]
